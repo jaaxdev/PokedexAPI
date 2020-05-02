0dacd6de989b01b9dc789db532b4e00d9c9165b7
jaaziel <jaaziel.irc@gmail.com>
2020-05-01 19:17:08 -0500

    consumo API correcto

--- app/src/main/java/com/jaax/pokeapidex/MainActivity.java
+++ app/src/main/java/com/jaax/pokeapidex/MainActivity.java
@@ -3,12 +3,23 @@ package com.jaax.pokeapidex;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 
+import com.jaax.pokeapidex.models.Pokemon;
+import com.jaax.pokeapidex.models.PokemonRespuesta;
+import com.jaax.pokeapidex.pokeapi.PokeapiService;
+
+import java.util.ArrayList;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class MainActivity extends AppCompatActivity {
     private Retrofit retrofit;
+    private static final String TAG = "POKEDEX";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,7 +27,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         retrofit = new Retrofit.Builder()
-                .baseUrl("https://www.pokeapi.co/api/v2/pokemon/")
+                .baseUrl("https://www.pokeapi.co/api/v2/")
                 .addConverterFactory(GsonConverterFactory.create())
                 .build();
 
@@ -25,6 +36,29 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void obtenerDatos(){
+        PokeapiService service = retrofit.create(PokeapiService.class);
+        Call<PokemonRespuesta> respuestaCall = service.obtenerListaPokemon();
 
+        respuestaCall.enqueue(new Callback<PokemonRespuesta>() {
+            @Override
+            public void onResponse(Call<PokemonRespuesta> call, Response<PokemonRespuesta> response) {
+                if(response.isSuccessful()){
+                    PokemonRespuesta respuesta = response.body();
+                    ArrayList<Pokemon> listaPokemon = respuesta.getResults();
+
+                    for(int i=0; i<listaPokemon.size(); i++){
+                        Pokemon p = listaPokemon.get(i);
+                        Log.i(TAG, "Pokemon: " + p.getName());
+                    }
+                } else {
+                    Log.e(TAG, " onResponse: "+response.errorBody());
+                }
+            }
+
+            @Override
+            public void onFailure(Call<PokemonRespuesta> call, Throwable t) {
+                Log.e(TAG, "onFailure: " + t.getMessage());
+            }
+        });
     }
 }
